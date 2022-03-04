61c571c12b609914aea7c730209cabe8540ce613
CHIMITA <smile0375-@naver.com>
2022-03-05 01:55:31 +0900

    Round Setting

--- APP_Dev/app/src/main/java/com/yeoulcom/power/MainActivity.java
+++ APP_Dev/app/src/main/java/com/yeoulcom/power/MainActivity.java
@@ -3,12 +3,25 @@ package com.yeoulcom.power;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.Z_GOBtn);
+        imageView = findViewById(R.id.BackImg);
+
+
+
     }
+
 }
